8c1bfcec8a6e7e3343b73adb76c264fe0ee767a1
github user name <tomo.zu_shima@icloud.com>
2022-09-15 16:47:56 +0900

    filterAdd

--- main.js
+++ main.js
@@ -19,7 +19,13 @@ var todoStorage = {
 const app = new Vue({
     el:'#app',
     data:{
-        todos:[]
+        todos:[],
+        options: [
+            { value: -1, label: 'すべて' },
+            { value: 0, label: '作業中' },
+            { value: 1, label: '完了' },
+        ],
+        current: -1
     },
     methods: {
         // ToDo 追加の処理
@@ -60,5 +66,14 @@ const app = new Vue({
     },
     created() {
         this.todos = todoStorage.fetch()
+    },
+    computed: {
+        computedTodos: function(){
+            // データ current が -1 ならすべて
+      // それ以外なら current と state が一致するものだけに絞り込む
+      return this.todos.filter(function(el){
+        return this.current < 0 ? true : this.current === el.state
+      },this)
+        }
     },
 })
